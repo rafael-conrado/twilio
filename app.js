7b22c1e682934c9bd56715ab77a6064d4b5bba76
Rafael Conrado <rafael070893@hotmail.com>
2022-03-09 17:18:14 -0300

    fix: credenciais com dot.env

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const accountSid = 'AC3644d62d15698ba8675c334f00877d9e', 
-      authToken = '', 
-      client = require('twilio')(accountSid, authToken); 
- 
-client.messages 
-      .create({ 
-         body: 'TwilioQuest rules',  
-         from: '+18065152415',      
-         to: '+5511987966688' 
-       }) 
-      .then(message => console.log(message.sid)) 
+require('dotenv').config();
+const accountSid = process.env.USERT
+const authToken = process.env.APIKEY
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+            body: 'TwilioQuest rules',
+            from: '+18065152415',
+            to: '+5511987966688'
+      })
+      .then(message => console.log(message.sid))
       .done();
